2903449074a07af181ced4e8e6cc89c60f69e707
Kyle Creek <Kyle.A.Creek@gmail.com>
2019-07-08 21:33:52 -0700

    I Think I got this whole thing working

--- scratch.py
+++ scratch.py
@@ -44,106 +44,102 @@ def response_path(path):
     
     
     # --- Validate Provided Path --- #
-    # Creates a list of all FILES in the directory
 
     # Establish the Current Workinf Directory and add the "Webroot"
     file_path = os.getcwd() + "\webroot"
-    #print("filepath", file_path)
 
-    #for file_ in os.listdir(file_path):
-    #    if os.path.isfile(file_path+file):
-    #        print(file_)
-    #    elif os.path.isdir(file_path+file):
-    #        print("Directory")
-    #f = []
-    #f2 = []
+    # Create an Empty List to store all file routes
+    all_files = []
 
-    #for (dirpath, dirnames, filenames) in walk(file_path):
-    #    f.append(filenames)
-    #for thing in f:
-    #    print(thing)
-
-
-    # Parse the Path for examination and comparison to content
-    # and determine if it is a file or path
+    for (dirname, dirnames, filenames) in walk(file_path):
+        for filename in filenames:
+            all_files.append(os.path.join(dirname, filename))
     
-    # Create a list of the Path, length of the list is greater than
-    # 2, then We know a directory has been passed
-    path_elements = path.split(".")
-    
-    # Case Statement to Handle Files
-    if len(path_elements) > 1:
-        print("This is a file", path_elements)
+    if (file_path + path) in all_files:
 
 
-    
-        # --- Determine Resource MimeType --- #
-        ### Note: File Resources typically appear as "xxx.yyy"
-        ### Note: This Section is Currently Working!!
-        # Create a dictionary of MIME Types for comparison
-        mime_dictionary = mimetypes.types_map
+        # Parse the Path for examination and comparison to content
+        # and determine if it is a file or path
         
-        # Compare the provided resource extension to existing extensions
-        # and return the value.
-
-        # Split the Path Provided to the Function
-        mime_check = path.split(".")
-        # Add the period back into the Check
-        mime_check = '.' + mime_check[-1]
+        # Create a list of the Path, length of the list is greater than
+        # 2, then We know a directory has been passed
+        path_elements = path.split(".")
         
-        # Assign the Mime Type once it is determined to be in the 
-        # Dictionary
-        try:
-            mime_type = mime_dictionary[mime_check]
-        except KeyError:
-            raise NotImplementedError
+        # Case Statement to Handle Files
+        if len(path_elements) > 1:
+            #print("This is a file", path_elements)
+
+
+        
+            # --- Determine Resource MimeType --- #
+            ### Note: File Resources typically appear as "xxx.yyy"
+            ### Note: This Section is Currently Working!!
+            # Create a dictionary of MIME Types for comparison
+            mime_dictionary = mimetypes.types_map
             
-        # --- Map Data Based On Provided Path --- #
-        retrieve_path = file_path + path
-        print(retrieve_path)
+            # Compare the provided resource extension to existing extensions
+            # and return the value.
 
-        with open(retrieve_path, 'rb') as binary_file:
-            content = binary_file.read()
+            # Split the Path Provided to the Function
+            mime_check = path.split(".")
+            # Add the period back into the Check
+            mime_check = '.' + mime_check[-1]
+            
+            # Assign the Mime Type once it is determined to be in the 
+            # Dictionary
+            try:
+                mime_type = mime_dictionary[mime_check]
+            except KeyError:
+                raise NotImplementedError
+                
+            # --- Map Data Based On Provided Path --- #
+            retrieve_path = file_path + path
+            #print(retrieve_path)
+
+            with open(retrieve_path, 'rb') as binary_file:
+                content = binary_file.read()
+            
+
+            # --- Turn Resource into Bytes --- # 
+
+            
+
+            # TODO: Fill in the appropriate content and mime_type give the path.
+            # See the assignment guidelines for help on "mapping mime-types", though
+            # you might need to create a special case for handling make_time.py
+            #
+            # If the path is "make_time.py", then you may OPTIONALLY return the
+            # result of executing `make_time.py`. But you need only return the
+            # CONTENTS of `make_time.py`.
+            
+            #content = b"not implemented"
+            #mime_type = b"not implemented"
+
         
+        # Case Statement to Handle Directories
+        ### Note: This section currently Works
+        else:
+            # Re-Route the File Path to return Text of Directory
+            get_path = file_path + path
 
-        # --- Turn Resource into Bytes --- # 
+            # Obtain all the content from that directory
+            contents = os.listdir(get_path)
 
-        
+            # Write all the content as a bit string
+            content = ''
+            for data in contents:
+                content = content + data + '\r\n'
+            content = content.encode('utf-8')
+            #print(content)
+            mime_type = b"text/plain"
 
-        # TODO: Fill in the appropriate content and mime_type give the path.
-        # See the assignment guidelines for help on "mapping mime-types", though
-        # you might need to create a special case for handling make_time.py
-        #
-        # If the path is "make_time.py", then you may OPTIONALLY return the
-        # result of executing `make_time.py`. But you need only return the
-        # CONTENTS of `make_time.py`.
-        
-        #content = b"not implemented"
-        #mime_type = b"not implemented"
-
-    
-    # Case Statement to Handle Directories
-    ### Note: This section currently Works
     else:
-        # Re-Route the File Path to return Text of Directory
-        get_path = file_path + path
-
-        # Obtain all the content from that directory
-        contents = os.listdir(get_path)
-
-        # Write all the content as a bit string
-        content = ''
-        for data in contents:
-            content = content + data + '\r\n'
-        content = content.encode('utf-8')
-        #print(content)
-        mime_type = b"text/plain"
-
-
+        content = b"Not Implemented"
+        mime_type=b"Not Implemented"
     return content, mime_type
 
 
 paths = ['\\a_web_page.html', '\\favicon.ico','\\images\\','\\images\\sample_1.png']
 
 test = response_path('\\images\\sample_1.png')
-print("THis is test", test)
+#print("THis is test", test)
